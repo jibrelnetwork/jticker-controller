0da42af6808c5b0a5dd47e4b90e7c5e46821363b
pohmelie <multisosnooley@gmail.com>
2020-02-03 17:41:06 +0300

    support time series cluster

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -1,11 +1,11 @@
-import pytest
-import pickle
 import asyncio
+import pickle
 
+import pytest
 from aiohttp import ClientSession
 from async_timeout import timeout
 
-from jticker_core import Task, EPOCH_START, Candle, Interval, RawTradingPair
+from jticker_core import EPOCH_START, Candle, Interval, RawTradingPair, Task
 
 
 @pytest.mark.asyncio
@@ -86,29 +86,31 @@ async def test_get_candles(base_url, mocked_kafka, controller):
 
 @pytest.mark.asyncio
 async def test_storage_query(controller, client):
-    response = await client("GET", "storage/query?query=show%20databases")
+    response = await client("GET", "storage/localhost/query?query=show%20databases")
     assert response["status"] == "ok"
     result = response["result"]
     assert result["0"] == []
-    await client("GET", "storage/query?query=create%20database%20foo")
-    response = await client("GET", "storage/query?query=show%20databases")
+    await client("GET", "storage/localhost/query?query=create%20database%20foo")
+    response = await client("GET", "storage/localhost/query?query=show%20databases")
     assert response["status"] == "ok"
     result = response["result"]
     assert result["0"] == [{"name": "foo"}]
 
-    response = await client("GET", "storage/query", _raw=True)
+    response = await client("GET", "storage/localhost/query", _raw=True)
     assert response.status == 400
 
-    response = await client("GET", "storage/query?query=foobar")
+    response = await client("GET", "storage/localhost/query?query=foobar")
     assert response["status"] == "error"
 
 
 @pytest.mark.asyncio
 async def test_migration(controller, client):
-    await client("GET", "storage/migrate", _raw=True)
+    await client("GET", "storage/localhost/migrate", _raw=True)
     async with timeout(10):
         while True:
             await asyncio.sleep(0.1)
-            names = set(await controller.time_series.client.show_databases())
+            assert len(controller._time_series_by_host) == 1
+            ts, *_ = controller._time_series_by_host.values()
+            names = set(await ts.client.show_databases())
             if names == {"service", "candles"}:
                 break
